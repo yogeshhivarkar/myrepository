77ed5736332edfbcdbcec7f9a2d2578bd2ed85b9
yogesh sudhakar hivarkar <yogesh.hivarkar@s-force.org>
2019-12-01 08:25:43 +0530

    session added

---
+++ SessionFactoryRegistry.java
@@ -0,0 +1,30 @@
+package com.gvl.helper;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactoryRegistry {
+	private static SessionFactory sessionFactory;
+
+	static {
+		try {
+			sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure().build())
+					.getMetadataBuilder().build().buildSessionFactory();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void closeSessionFactory() {
+		if (sessionFactory != null) {
+			sessionFactory.close();
+			sessionFactory = null;
+		}
+	}
+
+}
